f73a36d1e96e6cdb5b1334fdabedeb269ef532b9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-09 17:14:50 +0100

    Add workflow templates
    [ghstack-poisoned]

--- modules/devenv/github.nix
+++ modules/devenv/github.nix
@@ -21,6 +21,12 @@ in
       description = "Package to use for GitHub Actions";
     };
 
+    templates = {
+      automata.enable = mkEnableOption "Generate built-in automata workflow";
+      sapling.enable = mkEnableOption "Generate built-in sapling workflow";
+      check.enable = mkEnableOption "Generate built-in check workflow";
+    };
+
     workflows = mkOption {
       type = types.attrsOf (
         types.submodule {
@@ -86,6 +92,141 @@ in
           ".github/workflows/${name}.yaml".yaml = workflowCfg.settings;
         }
       ) cfg.workflows)
+
+      (mkIf cfg.templates.automata.enable {
+        ".github/workflows/automata.yaml".yaml = {
+          name = "Update";
+          on = {
+            schedule = [ { cron = "0 0 * * 0"; } ];
+            workflow_dispatch = null;
+          };
+          jobs.update = {
+            runs-on = "ubuntu-latest";
+            steps = [
+              {
+                id = "createGithubAppToken";
+                uses = "actions/create-github-app-token@v2";
+                "with" = {
+                  app-id = "\${{ vars.OPERATOR_APP_ID }}";
+                  private-key = "\${{ secrets.OPERATOR_PRIVATE_KEY }}";
+                };
+              }
+              {
+                uses = "actions/checkout@v5";
+                "with" = {
+                  fetch-depth = 0;
+                  token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                uses = "shikanime-studio/setup-nix-action@v1";
+                "with" = {
+                  github-token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                uses = "shikanime-studio/automata-action@v1";
+                "with" = {
+                  ghstack-username = "operator6o";
+                  github-token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                  gpg-passphrase = "\${{ secrets.GPG_PASSPHRASE }}";
+                  gpg-private-key = "\${{ secrets.GPG_PRIVATE_KEY }}";
+                  sign-commits = true;
+                  username = "Operator 6O <operator6o@shikanime.studio>";
+                };
+              }
+            ];
+          };
+        };
+      })
+
+      (mkIf cfg.templates.sapling.enable {
+        ".github/workflows/sapling.yaml".yaml = {
+          name = "Land";
+          on.issue_comment.types = [ "created" ];
+          jobs.land = {
+            runs-on = "ubuntu-latest";
+            steps = [
+              {
+                id = "createGithubAppToken";
+                uses = "actions/create-github-app-token@v2";
+                "with" = {
+                  app-id = "\${{ vars.OPERATOR_APP_ID }}";
+                  private-key = "\${{ secrets.OPERATOR_PRIVATE_KEY }}";
+                };
+              }
+              {
+                uses = "actions/checkout@v5";
+                "with" = {
+                  fetch-depth = 0;
+                  token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                uses = "shikanime-studio/setup-nix-action@v1";
+                "with" = {
+                  github-token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                uses = "shikanime-studio/sapling-action@v4";
+                "with" = {
+                  github-token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                  gpg-passphrase = "\${{ secrets.GPG_PASSPHRASE }}";
+                  gpg-private-key = "\${{ secrets.GPG_PRIVATE_KEY }}";
+                  sign-commits = true;
+                  username = "Operator 6O <operator6o@shikanime.studio>";
+                };
+              }
+            ];
+          };
+        };
+      })
+
+      (mkIf cfg.templates.check.enable {
+        ".github/workflows/check.yaml".yaml = {
+          name = "Check";
+          on = {
+            push.branches = [ "main" ];
+            pull_request.branches = [ "main" "gh/*/*/base" ];
+          };
+          jobs.check = {
+            runs-on = "ubuntu-latest";
+            steps = [
+              {
+                id = "createGithubAppToken";
+                uses = "actions/create-github-app-token@v2";
+                "with" = {
+                  app-id = "\${{ vars.OPERATOR_APP_ID }}";
+                  private-key = "\${{ secrets.OPERATOR_PRIVATE_KEY }}";
+                };
+              }
+              {
+                uses = "actions/checkout@v5";
+                "with" = {
+                  fetch-depth = 0;
+                  token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                uses = "shikanime-studio/setup-nix-action@v1";
+                "with" = {
+                  github-token = "\${{ steps.createGithubAppToken.outputs.token }}";
+                };
+              }
+              {
+                name = "Check Nix Flake";
+                run = ''
+                  nix flake check \
+                    --accept-flake-config \
+                    --all-systems \
+                    --no-pure-eval
+                '';
+              }
+            ];
+          };
+        };
+      })
     ];
 
     git-hooks.hooks.actionlint.enable = true;
