ffcbc2ef6bdfff4de871a6221361acd93207f27f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-11 21:14:35 +0100

    None on "Add Flake module"
    [ghstack-poisoned]

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -1,9 +1,36 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.devlib;
+in
 {
-  perSystem =
-    { config, ... }:
-    {
-      devenv.modules = [ ../devenv/default.nix ];
-      pre-commit.settings = config.devenv.shells.default.git-hooks;
-      treefmt = config.devenv.shells.default.treefmt.config;
+  options.devlib = {
+    pre-commit.shell = mkOption {
+      type = types.str;
+      default = "default";
+      description = "Shell name to read pre-commit git-hooks configuration from.";
     };
+
+    treefmt.shell = mkOption {
+      type = types.str;
+      default = "default";
+      description = "Shell name to read treefmt configuration from.";
+    };
+  };
+
+  config = {
+    perSystem =
+      { config, ... }:
+      let
+        inherit (config.devenv) shells;
+        getShell = name: if hasAttr name shells then shells.${name} else shells.default;
+      in
+      {
+        devenv.modules = [ ../devenv/default.nix ];
+        pre-commit.settings = (getShell cfg.pre-commit.shell).git-hooks;
+        treefmt = (getShell cfg.treefmt.shell).treefmt.config;
+      };
+  };
 }
