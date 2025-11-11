0ad07fb90dbb155e1eb25c9437a6bae0da8cad02
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-11 18:09:10 +0100

    Add Buf
    ghstack-source-id: 47a9d2b73ce1f92a84ecd6f5af3b0616298f507f
    Pull Request resolved: https://github.com/shikanime-studio/devlib/pull/35

---
+++ modules/devenv/buf.nix
@@ -0,0 +1,96 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.buf;
+  yamlFormat = pkgs.formats.yaml { };
+
+  resolvePlugin =
+    pluginCfg:
+    if pluginCfg ? package then
+      let
+        exe = getExe pluginCfg.package;
+      in
+      (removeAttrs pluginCfg [
+        "package"
+        "plugin"
+        "remote"
+      ])
+      // {
+        local = exe;
+      }
+    else
+      pluginCfg;
+
+  genResolved =
+    if cfg.generate ? plugins then
+      cfg.generate // { plugins = map resolvePlugin cfg.generate.plugins; }
+    else
+      cfg.generate;
+in
+{
+  options.buf = {
+    enable = mkEnableOption "Buf configuration generator";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.buf;
+      description = "Buf CLI package to expose in the dev shell.";
+    };
+
+    generate = mkOption {
+      type = types.submodule {
+        freeformType = yamlFormat.type;
+      };
+      default = {
+        version = "v2";
+        plugins = [ ];
+      };
+      description = "Contents of buf.gen.yaml.";
+      example = literalExpression ''
+        {
+          version = "v2";
+          managed = { enabled = true; };
+          plugins = [
+            {
+              # Remote plugin
+              plugin = "buf.build/protocolbuffers/go";
+              out = "gen/go";
+            }
+            {
+              # Local plugin resolved from a Nix package
+              package = pkgs.protoc-gen-go;
+              out = "gen/go";
+            }
+          ];
+        }
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = [ cfg.package ];
+
+    files = {
+      "buf.gen.yaml".yaml = genResolved;
+    };
+
+    gitignore.content = [
+      "buf.gen.yaml"
+    ];
+
+    tasks."devlib:buf:generate" = {
+      description = "Run buf generate with buf.gen.yaml";
+      before = [ "devenv:enterShell" ];
+      exec = ''
+        ${getExe cfg.package} generate
+      '';
+    };
+  };
+}
--- modules/devenv/default.nix
+++ modules/devenv/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./air.nix
+    ./buf.nix
     ./deno.nix
     ./git-hooks.nix
     ./github.nix
