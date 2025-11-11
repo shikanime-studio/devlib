a7bff1ce2da2c9ca47e75eeda6338535b5c143cb
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-11 17:22:30 +0100

    Add Buf
    [ghstack-poisoned]

---
+++ modules/devenv/buf.nix
@@ -0,0 +1,85 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.buf;
+  yamlFormat = pkgs.formats.yaml { };
+in
+{
+  options.buf = {
+    enable = mkEnableOption "Buf configuration generator";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.buf;
+      description = "Buf CLI package to expose in the dev shell.";
+    };
+
+    config = mkOption {
+      type = types.submodule {
+        freeformType = yamlFormat.type;
+      };
+      default = {
+        version = "v2";
+      };
+      description = "Contents of buf.yaml.";
+      example = literalExpression ''
+        {
+          version = "v2";
+          deps = [ "buf.build/googleapis/googleapis" ];
+          breaking = { use = [ "FILE" ]; };
+          lint = { use = [ "DEFAULT" ]; };
+        }
+      '';
+    };
+
+    gen = mkOption {
+      type = types.submodule {
+        freeformType = yamlFormat.type;
+      };
+      default = {
+        version = "v2";
+        plugins = [ ];
+      };
+      description = "Contents of buf.gen.yaml.";
+      example = literalExpression ''
+        {
+          version = "v2";
+          managed = {
+            enabled = true;
+          };
+          plugins = [
+            {
+              plugin = "buf.build/protocolbuffers/go";
+              out = "gen/go";
+            }
+            {
+              plugin = "buf.build/grpc/go";
+              out = "gen/go";
+            }
+          ];
+        }
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = [ cfg.package ];
+
+    files = {
+      "buf.yaml".yaml = cfg.config;
+      "buf.gen.yaml".yaml = cfg.gen;
+    };
+
+    gitignore.content = [
+      "buf.yaml"
+      "buf.gen.yaml"
+    ];
+  };
+}
