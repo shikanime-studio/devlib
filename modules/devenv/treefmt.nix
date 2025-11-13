e6aac37c35f05f579ac4f1b8267f36ae01889caa
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-13 20:39:04 +0100

    Fix treefmt unused vars
    ghstack-source-id: 468b8f8afc9bc691712e32d444915936b088e5d1
    Pull Request resolved: https://github.com/shikanime-studio/devlib/pull/45

--- modules/devenv/treefmt.nix
+++ modules/devenv/treefmt.nix
@@ -10,34 +10,8 @@ with lib;
 let
   cfg = config.treefmt;
 
-  inputArgs = {
-    name = "treefmt-nix";
-    url = "github:numtide/treefmt-nix";
-    attribute = "treefmt";
-    follows = [ "nixpkgs" ];
-  };
-
-  # When enabled, use getInput (throws helpful error if missing)
-  # Otherwise, use tryGetInput to populate the docs when the input is available.
-  treefmt-nix =
-    if cfg.enable then config.lib.getInput inputArgs else config.lib.tryGetInput inputArgs;
-
-  treefmtSubmodule =
-    if treefmt-nix != null then
-      treefmt-nix.lib.submoduleWith lib {
-        specialArgs = { inherit pkgs; };
-      }
-    else
-      lib.types.attrs;
-
-  # Determine tree root: prefer git.root, fallback to devenv.root
   treeRoot = if config.git.root != null then config.git.root else config.devenv.root;
 
-  # Custom wrapper to point treefmt to the project root.
-  #
-  # treefmt-nix provides a `config.build.wrapper` that uses `config.projectRootFile` to find the tree root.
-  # We instead set the tree root directory based on the information provided by the CLI.
-  # If the user sets a custom `projectRootFile`, we'll use that.
   treefmtWrapper =
     let
       treeRootOption =
