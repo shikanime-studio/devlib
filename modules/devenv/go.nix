eca20a3cd7c49c38f683719e6b424c81c1b9f75f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-13 20:39:09 +0100

    Add go mod generation
    ghstack-source-id: a5b95f16f7577518878bb08fc2a6905e12b50fbe
    Pull Request resolved: https://github.com/shikanime-studio/devlib/pull/44

--- modules/devenv/go.nix
+++ modules/devenv/go.nix
@@ -19,6 +19,14 @@ in
         "tt:go"
       ];
     };
+    tasks."go:tidy" = {
+      exec = "${getExe cfg.package} mod tidy";
+      execIfModified = [
+        "**/*.go"
+        "go.mod"
+        "go.sum"
+      ];
+    };
     treefmt.config.programs = {
       gofmt.enable = true;
       golines.enable = true;
