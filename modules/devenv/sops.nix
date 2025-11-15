cd6d752584b02fce0fa7fac76b12b8b1cd674065
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-15 01:17:46 +0100

    Sops fail if enc is not found
    [ghstack-poisoned]

--- modules/devenv/sops.nix
+++ modules/devenv/sops.nix
@@ -30,7 +30,7 @@ in
         before = [ "devenv:enterShell" ];
         exec = ''
           find . -type f -name "*.enc.*" -print0 | while IFS= read -r -d '' f; do
-            ${pkgs.lib.getExe cfg.package} updatekeys "$f"
+            ${getExe cfg.package} updatekeys "$f"
           done
         '';
       };
