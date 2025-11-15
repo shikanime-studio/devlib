3843f3f908013fcbba0660eec6706a344337c8d5
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-15 01:06:45 +0100

    Add SOPS
    ghstack-source-id: 526f37ff4b5bfd07c53f6304e2a7c51f4f662120
    Pull Request resolved: https://github.com/shikanime-studio/devlib/pull/46

---
+++ modules/devenv/sops.nix
@@ -0,0 +1,45 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.sops;
+in
+{
+  options.sops = {
+    enable = mkEnableOption "Sops configuration generator";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.sops;
+      description = "Sops CLI package to expose in the dev shell.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = [ cfg.package ];
+
+    tasks = {
+      "devenv:treefmt:run".after = [ "devlib:sops:updatekeys" ];
+      "devlib:sops:updatekeys" = {
+        description = "Run sops updatekeys";
+        before = [ "devenv:enterShell" ];
+        exec = ''
+          for f in *.enc.*; do
+            ${getExe cfg.package} updatekeys "$f"
+          done
+        '';
+      };
+    };
+
+    treefmt.config.programs = {
+      prettier.enable = true;
+      taplo.enable = true;
+    };
+  };
+}
