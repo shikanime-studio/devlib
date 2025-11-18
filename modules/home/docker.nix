64d2202c9b3e7dea91406e1902f6f2606be9718e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-18 19:21:14 +0000

    Add docker-cli config
    Original commit changeset: 3cff2c532451

---
+++ modules/home/docker.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.programs.dokcer-cli;
+in
+{
+  config = mkIf (cfg.enable || cfg.rootless.enable) {
+    programs.docker-cli.configDir = "${config.xdg.configHome}/docker";
+
+    xdg.configFile."docker/config.json".source =
+      let
+        format = pkgs.formats.json { };
+      in
+      format.generate "config.json" {
+        auths = {
+          "ghcr.io" = { };
+        };
+        credsStore = "secretservice";
+      };
+  };
+}
