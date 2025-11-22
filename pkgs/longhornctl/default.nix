19140516876d3f89e60bedc4ed8cdbb6a463498e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-22 02:00:41 +0100

    None on "Fix Longhorn update relative to script"
    [ghstack-poisoned]

--- pkgs/longhornctl/default.nix
+++ pkgs/longhornctl/default.nix
@@ -8,15 +8,15 @@ pkgs.buildGoModule rec {
     owner = "longhornctl";
     repo = "cli";
     rev = "v${version}";
-    hash = "sha256-39WpihpqcvPDrQVqw9dx9djqaQd5WzO8+9YRbewn0AU=";
+    hash = "sha256-ETbUOnR+beIDL8T3JR9kzmzp+WejmslooqyLwIPX1rI=";
   };
 
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/longhornctl/cli/meta.Version=v${version}"
-    "-X github.com/longhornctl/cli/meta.GitCommit=${src.rev}"
-    "-X github.com/longhornctl/cli/meta.BuildDate=1970-01-01T00:00:00+00:00"
+    "-X github.com/longhorn/cli/meta.Version=v${version}"
+    "-X github.com/longhorn/cli/meta.GitCommit=${src.rev}"
+    "-X github.com/longhorn/cli/meta.BuildDate=1970-01-01T00:00:00+00:00"
   ];
 
   passthru.update = ./update.nu;
@@ -31,7 +31,7 @@ pkgs.buildGoModule rec {
 
   meta = with pkgs.lib; {
     description = "Longhorn command line tool";
-    homepage = "https://github.com/longhornctl/cli";
+    homepage = "https://github.com/longhorn/cli";
     license = licenses.asl20;
     maintainers = with maintainers; [ shikanime ];
     mainProgram = "longhornctl";
