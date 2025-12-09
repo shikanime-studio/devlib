37d3d7720ee63dbd21e5e108d3af39f468cb61d8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-09 04:30:06 +0100

    Add unix completion

--- modules/home/docker.nix
+++ modules/home/docker.nix
@@ -7,15 +7,21 @@
     pkgs.yaml-language-server
   ];
 
-  programs.docker-cli = {
-    enable = true;
-    configDir = "${config.xdg.configHome}/docker";
-    settings.auths = {
-      "asia.gcr.io" = { };
-      "eu.gcr.io" = { };
-      "gcr.io" = { };
-      "ghcr.io" = { };
-      "us.gcr.io" = { };
+  programs = {
+    docker-cli = {
+      enable = true;
+      configDir = "${config.xdg.configHome}/docker";
+      settings.auths = {
+        "asia.gcr.io" = { };
+        "eu.gcr.io" = { };
+        "gcr.io" = { };
+        "ghcr.io" = { };
+        "us.gcr.io" = { };
+      };
     };
+
+    nushell.extraConfig = ''
+      use ${pkgs.nu_scripts}/share/nu_scripts/modules/docker *
+    '';
   };
 }
