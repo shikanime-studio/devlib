879f97e70ff3fda4d5f74bb654a2530d325b52bc
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-09 14:40:55 +0100

    Refactor ou triage from integration
    ghstack-source-id: 057d4558fc4ffa7b566a8d40b57984b126f31bce
    Pull Request resolved: https://github.com/shikanime-studio/devlib/pull/97

--- modules/devenv/shikanime-studio.nix
+++ modules/devenv/shikanime-studio.nix
@@ -89,203 +89,284 @@ with lib;
   github = {
     enable = mkDefault true;
 
-    actions = with config.github.lib; {
-      bot-triage = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          PR_NUMBER = mkWorkflowRef "github.event.pull_request.number";
-        };
-        "if" = concatStringsSep " || " [
-          "github.event.pull_request.user.login == 'yorha-operator-6o[bot]'"
-          "github.event.pull_request.user.login == 'dependabot[bot]'"
-        ];
-        run = mkWorkflowRun [
-          "gh"
-          "pr"
-          "edit"
-          ''"$PR_NUMBER"''
-          "--add-label"
-          "dependencies"
-        ];
-      };
-
-      cleanup-pr = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          BASE_REF = mkWorkflowRef "github.base_ref";
-          HEAD_REF = mkWorkflowRef "github.head_ref";
-          REPO = mkWorkflowRef "github.repository";
-        };
-        "if" = "!contains(github.event.pull_request.labels.*.name, 'ghstack')";
-        run = mkWorkflowRun [
-          "git"
-          "push"
-          "origin"
-          "--delete"
-          ''"$HEAD_REF"''
-        ];
-      };
-
-      cleanup-ghstack = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          BASE_REF = mkWorkflowRef "github.base_ref";
-          HEAD_REF = mkWorkflowRef "github.head_ref";
-          REPO = mkWorkflowRef "github.repository";
-        };
-        "if" = "contains(github.event.pull_request.labels.*.name, 'ghstack')";
-        run = ''
-          for role in base head orig; do
-            git push origin --delete "''${HEAD_REF%/head}/$role" || true
-          done
-        '';
-      };
-
-      automata = {
-        uses = "shikanime-studio/automata-action@v1";
-        "with" = {
-          ghstack-username = "operator6o";
-          github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          gpg-passphrase = mkWorkflowRef "secrets.GPG_PASSPHRASE";
-          gpg-private-key = mkWorkflowRef "secrets.GPG_PRIVATE_KEY";
-          sign-commits = true;
-          username = "Operator 6O <operator6o@shikanime.studio>";
-        };
-      };
-
-      create-github-app-token = {
-        id = "createGithubAppToken";
-        uses = "actions/create-github-app-token@v2";
-        "with" = {
-          app-id = mkWorkflowRef "vars.OPERATOR_APP_ID";
-          private-key = mkWorkflowRef "secrets.OPERATOR_PRIVATE_KEY";
-        };
-      };
-
-      create-release = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          REF_NAME = mkWorkflowRef "github.ref_name";
-          REPO = mkWorkflowRef "github.repository";
-        };
-        run = mkWorkflowRun [
-          "gh"
-          "release"
-          "create"
-          ''"$REF_NAME"''
-          "--repo"
-          ''"$REPO"''
-          "--generate-notes"
-        ];
-      };
-
-      checkout = {
-        uses = "actions/checkout@v6";
-        "with" = {
-          fetch-depth = 0;
-          token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-        };
-      };
-
-      comment-land = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "secrets.GITHUB_TOKEN";
-          PR_HTML_URL = mkWorkflowRef "github.event.issue.pull_request.html_url";
-        };
-        run = mkWorkflowRun [
-          "gh"
-          "pr"
-          "comment"
-          ''"$PR_HTML_URL"''
-          "--body"
-          ".land"
-        ];
-      };
-
-      direnv.uses = "shikanime-studio/direnv-action@v2";
-
-      docker-login = {
-        env = {
-          DOCKER_REGISTRY = "ghcr.io";
-          GITHUB_TOKEN = mkWorkflowRef "secrets.GITHUB_TOKEN";
-          USERNAME = mkWorkflowRef "github.actor";
-        };
-        run = mkWorkflowRun [
-          "nix"
-          "run"
-          "nixpkgs#docker"
-          "--"
-          "login"
-          ''"$DOCKER_REGISTRY"''
-          "--username"
-          ''"$USERNAME"''
-          "--password"
-          ''"$GITHUB_TOKEN"''
-        ];
-      };
-
-      ghstack-triage = {
-        env = {
-          GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          PR_NUMBER = mkWorkflowRef "github.event.pull_request.number";
-        };
-        "if" = concatStringsSep " && " [
+    actions =
+      with config.github.lib;
+      let
+        ghstackCondition = concatStringsSep " && " [
           "startsWith(github.head_ref, 'gh/')"
           "endsWith(github.head_ref, '/head')"
         ];
-        run = mkWorkflowRun [
-          "gh"
-          "pr"
-          "edit"
-          ''"$PR_NUMBER"''
-          "--add-label"
-          "ghstack"
+
+        mergeCondition = concatStringsSep " || " [
+          "github.event.pull_request.user.login == 'dependabot[bot]'"
+          "github.event.pull_request.user.login == 'yorha-operator-6o[bot]'"
         ];
-      };
+      in
+      {
+        automata = {
+          uses = "shikanime-studio/automata-action@v1";
+          "with" = {
+            ghstack-username = "operator6o";
+            github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            gpg-passphrase = mkWorkflowRef "secrets.GPG_PASSPHRASE";
+            gpg-private-key = mkWorkflowRef "secrets.GPG_PRIVATE_KEY";
+            sign-commits = true;
+            username = "Operator 6O <operator6o@shikanime.studio>";
+          };
+        };
 
-      nix-flake-check.run = mkWorkflowRun [
-        "nix"
-        "flake"
-        "check"
-        "--accept-flake-config"
-        "--all-systems"
-        "--no-pure-eval"
-      ];
+        cleanup-pr = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            BASE_REF = mkWorkflowRef "github.base_ref";
+            HEAD_REF = mkWorkflowRef "github.head_ref";
+            REPO = mkWorkflowRef "github.repository";
+          };
+          "if" = "!(${ghstackCondition})";
+          run = mkWorkflowRun [
+            "git"
+            "push"
+            "origin"
+            "--delete"
+            ''"$HEAD_REF"''
+          ];
+        };
 
-      nix-fmt.run = mkWorkflowRun [
-        "nix"
-        "fmt"
-      ];
+        cleanup-ghstack = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            BASE_REF = mkWorkflowRef "github.base_ref";
+            HEAD_REF = mkWorkflowRef "github.head_ref";
+            REPO = mkWorkflowRef "github.repository";
+          };
+          "if" = ghstackCondition;
+          run = ''
+            for role in base head orig; do
+              git push origin --delete "''${HEAD_REF%/head}/$role" || true
+            done
+          '';
+        };
 
-      sapling = {
-        uses = "shikanime-studio/sapling-action@v5";
-        "with" = {
-          github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          gpg-passphrase = mkWorkflowRef "secrets.GPG_PASSPHRASE";
-          gpg-private-key = mkWorkflowRef "secrets.GPG_PRIVATE_KEY";
-          sign-commits = true;
-          username = "Operator 6O <operator6o@shikanime.studio>";
+        create-github-app-token = {
+          id = "createGithubAppToken";
+          uses = "actions/create-github-app-token@v2";
+          "with" = {
+            app-id = mkWorkflowRef "vars.OPERATOR_APP_ID";
+            private-key = mkWorkflowRef "secrets.OPERATOR_PRIVATE_KEY";
+          };
+        };
+
+        create-release = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            REF_NAME = mkWorkflowRef "github.ref_name";
+            REPO = mkWorkflowRef "github.repository";
+          };
+          run = mkWorkflowRun [
+            "gh"
+            "release"
+            "create"
+            ''"$REF_NAME"''
+            "--repo"
+            ''"$REPO"''
+            "--generate-notes"
+          ];
+        };
+
+        checkout = {
+          uses = "actions/checkout@v6";
+          "with" = {
+            fetch-depth = 0;
+            token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+          };
+        };
+
+        comment-land-ghstack = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "secrets.GITHUB_TOKEN";
+            PR_HTML_URL = mkWorkflowRef "github.event.pull_request.html_url";
+          };
+          "if" = concatStringsSep " && " [
+            "(${mergeCondition})"
+            "(${ghstackCondition})"
+          ];
+          run = mkWorkflowRun [
+            "gh"
+            "pr"
+            "comment"
+            ''"$PR_HTML_URL"''
+            "--body"
+            ".land"
+            "|"
+            "ghstack"
+          ];
+        };
+
+        comment-land-pr = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "secrets.GITHUB_TOKEN";
+            PR_HTML_URL = mkWorkflowRef "github.event.pull_request.html_url";
+          };
+          "if" = concatStringsSep " && " [
+            "(${mergeCondition})"
+            "!(${ghstackCondition})"
+          ];
+          run = mkWorkflowRun [
+            "gh"
+            "pr"
+            "comment"
+            ''"$PR_HTML_URL"''
+            "--body"
+            ".land"
+            "|"
+            "pr"
+          ];
+        };
+
+        direnv.uses = "shikanime-studio/direnv-action@v2";
+
+        docker-login = {
+          env = {
+            DOCKER_REGISTRY = "ghcr.io";
+            GITHUB_TOKEN = mkWorkflowRef "secrets.GITHUB_TOKEN";
+            USERNAME = mkWorkflowRef "github.actor";
+          };
+          run = mkWorkflowRun [
+            "nix"
+            "run"
+            "nixpkgs#docker"
+            "--"
+            "login"
+            ''"$DOCKER_REGISTRY"''
+            "--username"
+            ''"$USERNAME"''
+            "--password"
+            ''"$GITHUB_TOKEN"''
+          ];
+        };
+
+        nix-flake-check.run = mkWorkflowRun [
+          "nix"
+          "flake"
+          "check"
+          "--accept-flake-config"
+          "--all-systems"
+          "--no-pure-eval"
+        ];
+
+        sapling = {
+          uses = "shikanime-studio/sapling-action@v5";
+          "with" = {
+            github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            gpg-passphrase = mkWorkflowRef "secrets.GPG_PASSPHRASE";
+            gpg-private-key = mkWorkflowRef "secrets.GPG_PRIVATE_KEY";
+            sign-commits = true;
+            username = "Operator 6O <operator6o@shikanime.studio>";
+          };
+        };
+
+        setup-nix = {
+          uses = "shikanime-studio/setup-nix-action@v1";
+          "with".github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+        };
+
+        stale = {
+          uses = "actions/stale@v10";
+          "with" = {
+            days-before-close = 14;
+            days-before-stale = 30;
+            repo-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            stale-issue-label = "stale";
+            stale-pr-label = "stale";
+          };
+        };
+
+        triage-bot = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            PR_NUMBER = mkWorkflowRef "github.event.pull_request.number";
+          };
+          "if" = mergeCondition;
+          run = mkWorkflowRun [
+            "gh"
+            "pr"
+            "edit"
+            ''"$PR_NUMBER"''
+            "--add-label"
+            "dependencies"
+          ];
+        };
+
+        triage-ghstack = {
+          env = {
+            GITHUB_TOKEN = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
+            PR_NUMBER = mkWorkflowRef "github.event.pull_request.number";
+          };
+          "if" = ghstackCondition;
+          run = mkWorkflowRun [
+            "gh"
+            "pr"
+            "edit"
+            ''"$PR_NUMBER"''
+            "--add-label"
+            "ghstack"
+          ];
         };
       };
-
-      setup-nix = {
-        uses = "shikanime-studio/setup-nix-action@v1";
-        "with".github-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-      };
-
-      stale = {
-        uses = "actions/stale@v10";
-        "with" = {
-          days-before-close = 14;
-          days-before-stale = 30;
-          repo-token = mkWorkflowRef "steps.createGithubAppToken.outputs.token";
-          stale-issue-label = "stale";
-          stale-pr-label = "stale";
-        };
-      };
-    };
 
     workflows = with config.github.lib; {
+      cleanup = {
+        enable = mkDefault true;
+        settings = {
+          name = "Cleanup";
+          on.pull_request.types = [
+            "closed"
+          ];
+          jobs.cleanup = {
+            runs-on = "ubuntu-latest";
+            steps = with config.github.actions; [
+              create-github-app-token
+              checkout
+              cleanup-pr
+              cleanup-ghstack
+            ];
+          };
+        };
+      };
+
+      integration = {
+        enable = mkDefault true;
+        settings = {
+          name = "Integration";
+          on.pull_request.branches = [
+            "main"
+            "gh/*/*/base"
+          ];
+          jobs = {
+            check = {
+              runs-on = "ubuntu-latest";
+              steps = with config.github.actions; [
+                create-github-app-token
+                checkout
+                setup-nix
+                nix-flake-check
+              ];
+            };
+            merge = {
+              needs = [ "check" ];
+              runs-on = "ubuntu-latest";
+              steps = with config.github.actions; [
+                create-github-app-token
+                checkout
+                setup-nix
+                comment-land-ghstack
+                comment-land-pr
+              ];
+            };
+          };
+        };
+      };
+
       land = {
         enable = mkDefault true;
         settings = {
@@ -325,49 +406,6 @@ with lib;
         };
       };
 
-      integration = {
-        enable = mkDefault true;
-        settings = {
-          name = "Integration";
-          on.pull_request.branches = [
-            "main"
-            "gh/*/*/base"
-          ];
-          jobs = {
-            triage = {
-              runs-on = "ubuntu-latest";
-              steps = with config.github.actions; [
-                create-github-app-token
-                checkout
-                bot-triage
-                ghstack-triage
-              ];
-            };
-            check = {
-              needs = [ "triage" ];
-              runs-on = "ubuntu-latest";
-              steps = with config.github.actions; [
-                create-github-app-token
-                checkout
-                setup-nix
-                nix-flake-check
-              ];
-            };
-            merge = {
-              "if" = "contains(github.event.pull_request.labels.*.name, 'auto')";
-              needs = [ "check" ];
-              runs-on = "ubuntu-latest";
-              steps = with config.github.actions; [
-                create-github-app-token
-                checkout
-                setup-nix
-                comment-land
-              ];
-            };
-          };
-        };
-      };
-
       release = {
         enable = mkDefault true;
         settings = {
@@ -397,20 +435,25 @@ with lib;
         };
       };
 
-      cleanup = {
+      triage = {
         enable = mkDefault true;
         settings = {
-          name = "Cleanup";
-          on.pull_request.types = [
-            "closed"
+          name = "Triage";
+          on.pull_request.branches = [
+            "main"
+            "gh/*/*/base"
           ];
-          jobs.cleanup = {
+          on.pull_request.types = [
+            "opened"
+            "reopened"
+          ];
+          jobs.triage = {
             runs-on = "ubuntu-latest";
             steps = with config.github.actions; [
               create-github-app-token
               checkout
-              cleanup-pr
-              cleanup-ghstack
+              triage-bot
+              triage-ghstack
             ];
           };
         };
@@ -431,7 +474,6 @@ with lib;
                 create-github-app-token
                 checkout
                 setup-nix
-                nix-fmt
                 automata
               ];
             };
