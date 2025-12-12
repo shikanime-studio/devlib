8d0e680b5b0809ee7e36330ff249b8eb677dc10a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-12 18:07:12 +0000

    Move Docker to language

--- modules/devenv/default.nix
+++ modules/devenv/default.nix
@@ -5,11 +5,11 @@
     ./integrations/automata.nix
     ./integrations/buf.nix
     ./integrations/golangci-lint.nix
-    ./integrations/docker.nix
     ./integrations/git-hooks.nix
     ./integrations/github.nix
     ./integrations/gitignore.nix
     ./integrations/sops.nix
+    ./languages/docker.nix
     ./languages/elixir.nix
     ./languages/go.nix
     ./languages/javascript.nix
