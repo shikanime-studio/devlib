bf28ab1be27ff699932d917688b8f8fc5d842827
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-14 20:19:17 +0000

    Add kind

--- modules/home/k8s.nix
+++ modules/home/k8s.nix
@@ -5,12 +5,15 @@
 }:
 
 {
-  home.packages = [
-    pkgs.kubectl
-    pkgs.kubernetes-helm
-    pkgs.kustomize
-    pkgs.skaffold
-  ];
+  home = {
+    packages = [
+      pkgs.kind
+      pkgs.kubectl
+      pkgs.kubernetes-helm
+      pkgs.kustomize
+      pkgs.skaffold
+    ];
 
-  home.sessionVariables.SKAFFOLD_CONFIG = "${config.xdg.configHome}/skaffold/config";
+    sessionVariables.SKAFFOLD_CONFIG = "${config.xdg.configHome}/skaffold/config";
+  };
 }
