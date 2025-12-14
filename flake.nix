d798bd7b887282e778579634370d0ed1e7a64adb
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-14 20:51:23 +0000

    Add Ocaml

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
           nix = ./modules/devenv/profiles/nix.nix;
           nixos = ./modules/devenv/shells/nixos.nix;
           opentofu = ./modules/devenv/profiles/opentofu.nix;
+          ocaml = ./modules/devenv/profiles/ocaml.nix;
           python = ./modules/devenv/profiles/python.nix;
           rust = ./modules/devenv/profiles/rust.nix;
           shell = ./modules/devenv/profiles/shell.nix;
---
+++ modules/devenv/profiles/ocaml.nix
@@ -0,0 +1,8 @@
+{
+  gitignore.templates = [
+    "tt:ocaml"
+  ];
+  languages.ocaml.enable = true;
+
+  treefmt.config.programs.ocamlformat.enable = true;
+}
