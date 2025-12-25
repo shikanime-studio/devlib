d4dc205866ac7288292b02768e2cc9009e54c8c8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-25 17:10:43 +0000

    Add GCC for CGO

--- modules/home/go.nix
+++ modules/home/go.nix
@@ -22,6 +22,8 @@
       telemetry.mode = "off";
     };
 
+    gcc.enable = true;
+
     nushell.extraConfig = ''
       source ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/godoc/godoc-completions.nu
     '';
