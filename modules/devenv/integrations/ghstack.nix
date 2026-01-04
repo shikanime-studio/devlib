9a9bea15e21c7dbab81a9a4b164bc50a1e4671d7
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-04 05:42:25 +0100

    Update
    [ghstack-poisoned]

--- modules/devenv/integrations/ghstack.nix
+++ modules/devenv/integrations/ghstack.nix
@@ -26,10 +26,20 @@ in
 
     packages = [ cfg.package ];
 
-    files.".git/hooks/pre-ghstack".text = ''
-      #!${pkgs.bashNonInteractive}/bin/bash
+    tasks."devlib:ghstack:hooks:install" = {
+      before = [ "devenv:enterShell" ];
+      description = "Install ghstack pre-ghstack hook";
+      exec =
+        let
+          hookScript = pkgs.writeScript "pre-ghstack" ''
+            #!${pkgs.bashNonInteractive}/bin/bash
 
-      ${getExe config.git-hooks.package} run --from-ref "$1" --to-ref "$2"
-    '';
+            ${getExe config.git-hooks.package} run --from-ref "$1" --to-ref "$2"
+          '';
+        in
+        ''
+          install -D -m 0755 ${hookScript} .git/hooks/pre-ghstack
+        '';
+    };
   };
 }
