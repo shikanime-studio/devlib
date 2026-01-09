84587cfbf6286fc631b690752ce6db66aaf365d0
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-09 17:27:22 +0100

    Fix missing Astro format match

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -50,20 +50,19 @@ in
                 treefmt.config.programs.prettier = withSystem system (
                   { config, pkgs, ... }:
                   {
+                    includes = [ "*.astro" ];
                     package = pkgs.prettier.override {
                       plugins = [
                         config.packages.prettier-plugin-astro
                         config.packages.prettier-plugin-tailwindcss
                       ];
                     };
-                    settings = {
-                      overrides = [
-                        {
-                          files = "*.astro";
-                          options.parser = "astro";
-                        }
-                      ];
-                    };
+                    settings.overrides = [
+                      {
+                        files = "*.astro";
+                        options.parser = "astro";
+                      }
+                    ];
                   }
                 );
               }
