cdfa91fbdf800ac2b51fba070211d48e98a19a27
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-14 20:35:56 +0100

    Update
    [ghstack-poisoned]

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,11 +1,37 @@
 {
   inputs = {
-    devenv.url = "github:cachix/devenv";
-    devlib.url = "github:shikanime-studio/devlib";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devlib = {
+      url = "github:shikanime-studio/devlib";
+      inputs = {
+        devenv.follows = "devenv";
+        flake-parts.follows = "flake-parts";
+        git-hooks.follows = "git-hooks";
+        nixpkgs.follows = "nixpkgs";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
