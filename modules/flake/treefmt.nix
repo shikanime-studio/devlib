142c6fe08f5b9e838765a9aa5aea9cf64f01e806
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-16 02:16:42 +0100

    Update
    [ghstack-poisoned]

--- modules/flake/treefmt.nix
+++ modules/flake/treefmt.nix
@@ -5,23 +5,28 @@ _:
   perSystem =
     { system, ... }:
     {
-      treefmt.config.programs.prettier = withSystem system (
-        { config, pkgs, ... }:
+      devenv.modules = [
         {
-          includes = [ "*.astro" ];
-          package = pkgs.prettier.override {
-            plugins = [
-              config.packages.prettier-plugin-astro
-              config.packages.prettier-plugin-tailwindcss
-            ];
-          };
-          settings.overrides = [
+          treefmt.config.programs.prettier = withSystem system (
+            { config, pkgs, ... }:
             {
-              files = "*.astro";
-              options.parser = "astro";
+              includes = [ "*.astro" ];
+              package = pkgs.prettier.override {
+                plugins = [
+                  config.packages.prettier-plugin-astro
+                  config.packages.prettier-plugin-tailwindcss
+                ];
+              };
+              settings.overrides = [
+                {
+                  files = "*.astro";
+                  options.parser = "astro";
+                }
+              ];
             }
-          ];
+          );
+
         }
-      );
+      ];
     };
 }
