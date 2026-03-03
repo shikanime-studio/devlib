b5ff98d84992e15654a5a024083d6585e4759171
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-03-03 23:46:28 +0100

    Update JavaScript to full Node package
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Ic3dd02cf69190aed36ad85c12e2c72396a6a6964
    ghstack-source-id: 5a6250d2523849eb703f010965b95507d36a5f1f
    Pull-Request: https://github.com/shikanime-studio/devlib/pull/214

--- modules/devenv/profiles/javascript.nix
+++ modules/devenv/profiles/javascript.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ ./base.nix ];
@@ -10,12 +10,12 @@
   languages.javascript = {
     enable = true;
     corepack.enable = true;
-
     npm = {
       install.enable = true;
       # FIXES: https://github.com/cachix/devenv/issues/2538
-      package = pkgs.nodejs-slim;
+      package = config.languages.javascript.package;
     };
+    package = pkgs.nodejs;
   };
 
   treefmt.config.settings.global.excludes = [ "node_modules/*" ];
