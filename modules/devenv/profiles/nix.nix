ea5085a8b06564bc3869900074dde8f1fb721729
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-03-23 23:26:58 +0100

    Update
    [ghstack-poisoned]

--- modules/devenv/profiles/nix.nix
+++ modules/devenv/profiles/nix.nix
@@ -224,13 +224,11 @@
       };
     };
 
-    integration.jobs = {
-      nix = {
-        uses = "./.github/workflows/nix.yaml";
-        secrets = {
-          OPERATOR_PRIVATE_KEY = "\${{ secrets.OPERATOR_PRIVATE_KEY }}";
-          CACHIX_AUTH_TOKEN = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
-        };
+    integration.jobs.nix = {
+      uses = "./.github/workflows/nix.yaml";
+      secrets = {
+        OPERATOR_PRIVATE_KEY = "\${{ secrets.OPERATOR_PRIVATE_KEY }}";
+        CACHIX_AUTH_TOKEN = "\${{ secrets.CACHIX_AUTH_TOKEN }}";
       };
     };
 
