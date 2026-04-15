2fbb60f84424a42132b3cfe7d6c10118b5bf0dff
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-15 19:06:55 +0200

    Reorder Skaffold config
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Ia9d6790ef81a4657e62e82f707c2b3256a6a6964
    ghstack-source-id: 80579a160ea907c19163831c6e16f12826a902df
    Pull-Request: https://github.com/shikanime-studio/devlib/pull/313

--- modules/devenv/profiles/skaffold.nix
+++ modules/devenv/profiles/skaffold.nix
@@ -7,10 +7,12 @@
 {
   imports = [ ./base.nix ];
 
+  env = {
+    SKAFFOLD_CACHE_FILE = config.env.DEVENV_STATE + "/skaffold/cache";
+    SKAFFOLD_REMOTE_CACHE_DIR = config.env.DEVENV_STATE + "/skaffold/remote-cache";
+  };
+
   github.workflows.skaffold.enable = true;
 
   packages = [ pkgs.skaffold ];
-
-  env.SKAFFOLD_CACHE_FILE = config.env.DEVENV_STATE + "/skaffold/cache";
-  env.SKAFFOLD_REMOTE_CACHE_DIR = config.env.DEVENV_STATE + "/skaffold/remote-cache";
 }
