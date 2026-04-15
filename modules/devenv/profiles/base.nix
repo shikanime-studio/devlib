d87d7e90e6b0e61d05529c6f65e10d45d6b9715d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-15 13:37:32 +0200

    Update
    [ghstack-poisoned]

--- modules/devenv/profiles/base.nix
+++ modules/devenv/profiles/base.nix
@@ -13,28 +13,49 @@
     config = {
       programs = {
         autocorrect.enable = true;
-        jsonfmt.enable = true;
+        oxfmt.enable = true;
         rumdl-check.enable = true;
-        rumdl-format.enable = true;
-        taplo.enable = true;
-        toml-sort.enable = true;
         xmllint.enable = true;
-        yamlfmt.enable = true;
       };
-      settings.global.excludes = [
-        ".devenv/*"
-        ".direnv/*"
-        "*.assetsignore"
-        "*.dockerignore"
-        "*.gcloudignore"
-        "*.gif"
-        "*.ico"
-        "*.jpg"
-        "*.png"
-        "*.svg"
-        "*.txt"
-        "*.webp"
-      ];
+      settings = {
+        formatter.oxfmt.includes = [
+          "*.cjs"
+          "*.css"
+          "*.graphql"
+          "*.hbs"
+          "*.html"
+          "*.js"
+          "*.json"
+          "*.json5"
+          "*.jsonc"
+          "*.jsx"
+          "*.md"
+          "*.mdx"
+          "*.mjs"
+          "*.mustache"
+          "*.scss"
+          "*.toml"
+          "*.ts"
+          "*.tsx"
+          "*.vue"
+          "*.yaml"
+          "*.yml"
+        ];
+        global.excludes = [
+          ".devenv/*"
+          ".direnv/*"
+          "*.assetsignore"
+          "*.dockerignore"
+          "*.gcloudignore"
+          "*.gif"
+          "*.ico"
+          "*.jpg"
+          "*.png"
+          "*.svg"
+          "*.txt"
+          "*.webp"
+        ];
+      };
     };
   };
 }
